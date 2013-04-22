81aa074e2aab36531b79fa06d4d95ad3c3db1c1d
itsmartin <github@martinbrook.com>
2013-04-22 13:55:49 +0100

    Initial commit of skeleton plugin

---
+++ src/com/martinbrook/DeathSwap/DeathSwap.java
@@ -0,0 +1,27 @@
+package com.martinbrook.DeathSwap;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DeathSwap extends JavaPlugin {
+	
+	public DeathSwap() {
+		// TODO Auto-generated constructor stub
+	}
+	
+    @Override
+    public void onEnable(){
+        // TODO Insert logic to be performed when the plugin is enabled
+    }
+ 
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
+    
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+    	// TODO
+		return false;
+    }
+}
